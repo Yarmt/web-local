4d1bd4dd237c15b090f993535897ed3d17d93144
Yarmt <shminke.yaroslav@yandex.ru>
2023-11-30 03:43:39 +0300

    first commit

---
+++ sait-php2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="css/style.css">
+    <link rel = "stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Мой веб сайт с php</title>
+</head>
+<body>
+<header>
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="htdocs\img\calc+.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Калькуляторы со встоеным ИИ</h1>
+        <p class="lead">Данный продукт уникален своим сочетанием привчыного нам иструмента, для расчёта математических выражений и современной, я бы даже сказал новейшей технологией искуственного интелекта. 
+        Этот крутой калькулятор с ИИ может решать даже самые сложные математические задачи за считанные секунды!
+        </p>
+      </div>
+    </div>
+</header>
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-4">Описание современного дизайна и вариативности калькулятора</h1>
+        <p class="col-lg-10 fs-4"><p>1.Калькулятор имеет современный дизайн с ярким и четким дисплеем, который обеспечивает удобство и комфорт в использовании.</p>
+<p>2. Кнопки калькулятора выполнены из высококачественных материалов, что гарантирует долговечность и надежность устройства.</p>
+<p>3. Калькулятор имеет множество функций, которые позволяют решать различные задачи, такие как научные расчеты, финансовые операции и т.д.</p>
+<p>4. Калькулятор может быть настроен на различные языки, что делает его удобным для использования в любой стране мира.</p>
+<p>5. Калькулятор имеет возможность сохранять историю расчетов, что позволяет быстро вернуться к предыдущим результатам.</p>
+      </div>
+      <div class="col-md-10 mx-auto col-lg-5">
+        <div>
+        <img src="htdocs\img\calc-brend.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="900" height="700" loading="lazy">
+      </div>
+    </div>
+  </div>
+<footer>
+    <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Your email</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Your password</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingText" placeholder="Text">
+            <label for="floatingText">Your message</label>
+          </div>
+          <div class="checkbox mb-3">
+            <label>
+            <input type="checkbox" value="remember-me"> Я согласен получать ежемесячную рассылку.
+            </label>
+            
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Send</button>
+          <hr class="my-4">
+          <small class="text-body-secondary">By clicking Send, you agree to the terms of use.</small>
+        </form>
+      </div>
+</footer>  
+</body>
+</html>
